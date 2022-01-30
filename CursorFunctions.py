3d198e82c833f4b30063f658a8934d9063e652eb
Matt Piehl <85842201+MattPiehl@users.noreply.github.com>
2022-01-29 21:49:46 -0700

    Update CursorFunctions.py

--- CursorFunctions.py
+++ CursorFunctions.py
@@ -3,9 +3,7 @@
 # I have managed to map the home button to the accelerometer - simply hold it and values will appear!
 
 # Coded by The Raspberry Pi Guy. Work based on some of Matt Hawkins's!
-import cwiid
-import time
-import turtle
+import cwiid, time, turtle
 
 button_delay = 0.1
 color = 1
@@ -15,7 +13,7 @@ time.sleep(1)
 try:
   wii=cwiid.Wiimote()
 
-t=turtle.Turtle()
+t = turtle.Turtle()
 circle_width = 5
 t.begin_fill()
 wii.rpt_mode = cwiid.RPT_BTN
@@ -26,35 +24,35 @@ while True:
     goto(200, 100)
     if (color == 1 & cwiid.BTN_A != True):
       colorRGB = 0,0,0
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)  
     else if (color == 2):
       colorRGB = 255,255,255
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 3):
       colorRGB = (0,0,255)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 4):
       colorRGB = (128,0,128)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 5):
       colorRGB = (255,192,203)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 6):
       colorRGB = (255,0,0)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 7):
       colorRGB = (255,255,0)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else if (color == 8):
       colorRGB = (0,255,0)
-      t.fillcolor(colorRGB)
+      turtle.fillcolor(colorRGB)
       circle(circle_width)
     else:
       color = 1
@@ -72,7 +70,7 @@ while True:
     time.sleep(button_delay)
 
   if (buttons & cwiid.BTN_A):
-    t.fillcolor(colorRGB)
+    turtle.fillcolor(colorRGB)
     circle(circle_width)
     time.sleep(button_delay)
 
