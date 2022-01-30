afee947fa1a19eb02c67746f5bf6dbd95463a1e0
Matt Piehl <85842201+MattPiehl@users.noreply.github.com>
2022-01-30 16:52:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ sudo apt-get install python-cwiid
 sudo apt-get install python3-matplotlib
 python -m pip install -U pip
 python -m pip install -U matplotlib
+echo 'Please hold the 1 and 2 buttons on your Wiimote now.'
 python CursorFunctions.py
