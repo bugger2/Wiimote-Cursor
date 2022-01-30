b2016964833d308b15fc1ca844bff523b552378d
Matt Piehl <85842201+MattPiehl@users.noreply.github.com>
2022-01-30 15:27:50 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ sudo apt-get update
 sudo apt-get install --no-install-recommends bluetooth -y
 sudo apt-get install python-cwiid
 sudo apt-get install python3-matplotlib
+python -m pip install -U pip
+python -m pip install -U matplotlib
 python CursorFunctions.py
