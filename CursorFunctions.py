61d1c048c8c6ec8cd0c38f06f9afb4ae289b330f
Matt Piehl <85842201+MattPiehl@users.noreply.github.com>
2022-02-02 17:55:41 -0700

    Update CursorFunctions.py

--- CursorFunctions.py
+++ CursorFunctions.py
@@ -35,7 +35,7 @@ turtle.fillcolor('#FFFFFF')
 turtle.circle(circle_width)
 while True:
   buttons = wii.state['buttons']
-  
+
   if (buttons - cwiid.BTN_PLUS - cwiid.BTN_MINUS == 0):
     print '\nClosing connection ...'
     # NOTE: This is how you RUMBLE the Wiimote
@@ -43,7 +43,7 @@ while True:
     time.sleep(1)
     wii.rumble = 0
     exit(wii)
-    
+
  # if (buttons & cwiid.BTN_A != True):
   #  turtle.pendown()
    # turtle.begin_fill()
@@ -71,38 +71,36 @@ while True:
     time.sleep(button_delay)
 #BUTTON B CODE
   if (buttons & cwiid.BTN_B):
-    while 2 == 2:
-      
-      turtle.pendown()
-      turtle.begin_fill()
-      turtle.fillcolor('#FFFFFF')
-      turtle.pencolor('#FFFFFF')
-      turtle.circle(circle_width)
-      turtle.end_fill()
-      turtle.clear()
-      wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
-      roll=(wii.state['acc'][0]-120)
-      pitch=(wii.state['acc'][1]-125)
-      check = 0
-      while check == 0:
-        if (roll < -10):
-          turtle.setheading(0)
-          turtle.forward(4)
-        if (roll > 10):
-          turtle.setheading(180)
-          turtle.forward(4)
-        if (pitch < -5):
-          turtle.setheading(90)
-          turtle.forward(4)
-        if (pitch > 10):
-          turtle.setheading(270)
-          turtle.forward(4)
-      time.sleep(0.01)
-      check = (buttons & cwiid.BTN_B)
-      time.sleep(button_delay)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.fillcolor('#FFFFFF')
+    turtle.pencolor('#FFFFFF')
+    turtle.circle(circle_width)
+    turtle.end_fill()
+    turtle.clear()
+    wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+    roll=(wii.state['acc'][0]-120)
+    pitch=(wii.state['acc'][1]-125)
+    check = 0
+    while check == 0:
+      if (roll < -10):
+        turtle.setheading(0)
+        turtle.forward(4)
+      if (roll > 10):
+        turtle.setheading(180)
+        turtle.forward(4)
+      if (pitch < -5):
+        turtle.setheading(90)
+        turtle.forward(4)
+      if (pitch > 10):
+        turtle.setheading(270)
+        turtle.forward(4)
+    time.sleep(button_delay)
+    check = (buttons & cwiid.BTN_B)
+    time.sleep(button_delay)
 
   if (buttons & cwiid.BTN_HOME):
     turtle.goto(200,200)
     time.sleep(button_delay)
-    
+
 turtle.endfill()
